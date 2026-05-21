c624f02a2a1866665133d1f3ac6d1a83d4ac9099
Leo <LeoTran@argosdyne.com>
2026-05-21 09:12:04 +0900

    Upgrade version to V01.04.00

--- src/QmlControls/QGroundControlQmlGlobal.cc
+++ src/QmlControls/QGroundControlQmlGlobal.cc
@@ -264,7 +264,7 @@ void QGroundControlQmlGlobal::setFlightMapZoom(double zoom)
 QString QGroundControlQmlGlobal::qgcVersion(void) const
 {
     //QString versionStr = qgcApp()->applicationVersion();
-    QString versionStr = "V02.03.00A";
+    QString versionStr = "V02.04.00";
 #ifdef __androidArm32__
     versionStr += QStringLiteral(" %1").arg(tr("32 bit"));
 #elif __androidArm64__
