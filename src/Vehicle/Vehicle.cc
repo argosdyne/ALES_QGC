b668487aabd7a982b93b7f901ecd2305c8045f0a
hienargosdyne <hien@argosdyne.com>
2026-07-28 11:57:53 +0900

    bug #9164
    [ALES QGC] V01.24.00 APM 4.5 Z01.04.01 , 켈리브레이션 중 오류 메시지 출력 후 중단 되는 상황 발생

--- src/Vehicle/Vehicle.cc
+++ src/Vehicle/Vehicle.cc
@@ -772,24 +772,6 @@ void Vehicle::_mavlinkMessageReceived(LinkInterface* link, mavlink_message_t mes
                    << "result" << ack.param_result;
         break;
     }
-    case MAVLINK_MSG_ID_STATUSTEXT: {
-        mavlink_statustext_t statusText;
-        mavlink_msg_statustext_decode(&message, &statusText);
-        const QString text = QString::fromLatin1(statusText.text, MAVLINK_MSG_STATUSTEXT_FIELD_TEXT_LEN).trimmed();
-        if (text.contains(QStringLiteral("ap_message"), Qt::CaseInsensitive) || text.contains(QStringLiteral("141"))) {
-            qWarning() << "[Mavlink141]" << "STATUSTEXT RX"
-                       << "vehicleId" << _id
-                       << "sysid" << static_cast<int>(message.sysid)
-                       << "compid" << static_cast<int>(message.compid)
-                       << "seq" << static_cast<int>(message.seq)
-                       << "len" << static_cast<int>(message.len)
-                       << "severity" << static_cast<int>(statusText.severity)
-                       << "link" << _vehicleLogLinkName(link)
-                       << _vehicleLogLinkPtr(link)
-                       << "text" << text;
-        }
-        break;
-    }
     case MAVLINK_MSG_ID_GIMBAL_MANAGER_INFORMATION:
         logMavlinkEndpoint("GIMBAL_MANAGER_INFORMATION");
         break;
@@ -808,36 +790,6 @@ void Vehicle::_mavlinkMessageReceived(LinkInterface* link, mavlink_message_t mes
     case MAVLINK_MSG_ID_CAMERA_SETTINGS:
         logMavlinkEndpoint("CAMERA_SETTINGS");
         break;
-    case MAVLINK_MSG_ID_ALTITUDE: {
-        static QMap<QString, qint64> lastAltitude141LogByEndpoint;
-        const QString key = QStringLiteral("%1:%2:%3")
-                .arg(_vehicleLogLinkPtr(link))
-                .arg(message.sysid)
-                .arg(message.compid);
-        const qint64 nowMSecs = QDateTime::currentMSecsSinceEpoch();
-        if (nowMSecs - lastAltitude141LogByEndpoint.value(key, 0) >= 2000) {
-            lastAltitude141LogByEndpoint[key] = nowMSecs;
-
-            mavlink_altitude_t altitude;
-            mavlink_msg_altitude_decode(&message, &altitude);
-            qWarning() << "[Mavlink141]" << "ALTITUDE RX"
-                       << "vehicleId" << _id
-                       << "sysid" << static_cast<int>(message.sysid)
-                       << "compid" << static_cast<int>(message.compid)
-                       << "seq" << static_cast<int>(message.seq)
-                       << "len" << static_cast<int>(message.len)
-                       << "link" << _vehicleLogLinkName(link)
-                       << _vehicleLogLinkPtr(link)
-                       << "time_usec" << altitude.time_usec
-                       << "monotonic" << altitude.altitude_monotonic
-                       << "amsl" << altitude.altitude_amsl
-                       << "local" << altitude.altitude_local
-                       << "relative" << altitude.altitude_relative
-                       << "terrain" << altitude.altitude_terrain
-                       << "bottom_clearance" << altitude.bottom_clearance;
-        }
-        break;
-    }
     default:
         break;
     }
@@ -2226,81 +2178,8 @@ bool Vehicle::sendMessageOnLinkThreadSafe(LinkInterface* link, mavlink_message_t
         return false;
     }
 
-    const auto logSetMessageIntervalTx = [this, link](const mavlink_message_t& mavlinkMessage, const char* stage) {
-        if (mavlinkMessage.msgid != MAVLINK_MSG_ID_COMMAND_LONG) {
-            return;
-        }
-
-        mavlink_command_long_t commandLong;
-        mavlink_msg_command_long_decode(&mavlinkMessage, &commandLong);
-        if (commandLong.command != MAV_CMD_SET_MESSAGE_INTERVAL) {
-            return;
-        }
-
-        qWarning() << "[Cmd511TX]"
-                   << stage
-                   << "vehicleId" << _id
-                   << "sysid" << static_cast<int>(mavlinkMessage.sysid)
-                   << "compid" << static_cast<int>(mavlinkMessage.compid)
-                   << "seq" << static_cast<int>(mavlinkMessage.seq)
-                   << "len" << static_cast<int>(mavlinkMessage.len)
-                   << "target" << QStringLiteral("%1/%2").arg(commandLong.target_system).arg(commandLong.target_component)
-                   << "p1" << commandLong.param1
-                   << "p2" << commandLong.param2
-                   << "p3" << commandLong.param3
-                   << "p4" << commandLong.param4
-                   << "p5" << commandLong.param5
-                   << "p6" << commandLong.param6
-                   << "p7" << commandLong.param7
-                   << "link" << _vehicleLogLinkName(link)
-                   << _vehicleLogLinkPtr(link);
-    };
-
-    if (message.msgid == MAVLINK_MSG_ID_ALTITUDE) {
-        mavlink_altitude_t altitude;
-        mavlink_msg_altitude_decode(&message, &altitude);
-        qWarning() << "[Mavlink141]" << "ALTITUDE TX"
-                   << "vehicleId" << _id
-                   << "sysid" << static_cast<int>(message.sysid)
-                   << "compid" << static_cast<int>(message.compid)
-                   << "seq" << static_cast<int>(message.seq)
-                   << "len" << static_cast<int>(message.len)
-                   << "link" << _vehicleLogLinkName(link)
-                   << _vehicleLogLinkPtr(link)
-                   << "time_usec" << altitude.time_usec
-                   << "monotonic" << altitude.altitude_monotonic
-                   << "amsl" << altitude.altitude_amsl
-                   << "local" << altitude.altitude_local
-                   << "relative" << altitude.altitude_relative
-                   << "terrain" << altitude.altitude_terrain
-                   << "bottom_clearance" << altitude.bottom_clearance;
-    }
-
-    if (message.msgid == MAVLINK_MSG_ID_COMMAND_LONG) {
-        mavlink_command_long_t commandLong;
-        mavlink_msg_command_long_decode(&message, &commandLong);
-        if (commandLong.command == MAV_CMD_SET_MESSAGE_INTERVAL && static_cast<int>(commandLong.param1) == MAVLINK_MSG_ID_ALTITUDE) {
-            qWarning() << "[Mavlink141]" << "SET_MESSAGE_INTERVAL TX"
-                       << "vehicleId" << _id
-                       << "sysid" << static_cast<int>(message.sysid)
-                       << "compid" << static_cast<int>(message.compid)
-                       << "seq" << static_cast<int>(message.seq)
-                       << "len" << static_cast<int>(message.len)
-                       << "targetSystem" << static_cast<int>(commandLong.target_system)
-                       << "targetComponent" << static_cast<int>(commandLong.target_component)
-                       << "requestedMsgId" << static_cast<int>(commandLong.param1)
-                       << "intervalUsec" << commandLong.param2
-                       << "link" << _vehicleLogLinkName(link)
-                       << _vehicleLogLinkPtr(link);
-        }
-    }
-
-    logSetMessageIntervalTx(message, "preAdjust");
-
     // Give the plugin a chance to adjust
     _firmwarePlugin->adjustOutgoingMavlinkMessageThreadSafe(this, link, &message);
-
-    logSetMessageIntervalTx(message, "postAdjust");
 
     // Write message into buffer, prepending start sign
     uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
